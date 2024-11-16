2c02163e430d3d31f0f74457d7745b1a0b5313c5
Boris Wang <boris.wang117@gmail.com>
2024-11-15 18:45:21 -0800

    Initalize repo

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "@/components/contact-form";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <ContactForm />
+    </>
+  );
+};
+
+export default page;
