3e3b93b05eda83c0e64dd9f4f1b1c9edb27a9cba
Boris Wang <boris.wang117@gmail.com>
2024-11-17 18:30:28 -0800

    Add header to layout and add navigation between pages

---
+++ src/app/account/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt corrupti
+      excepturi fugit mollitia dolorum, officiis blanditiis sapiente facere ipsa
+      neque optio dolorem ducimus necessitatibus sint libero ipsum eum dolore
+      numquam?
+    </div>
+  );
+};
+
+export default page;
