71b6e8ae9fa6a3a7aba87fcff11ee2b53229556c
ryanacharya <ryan2715@student.ubc.ca>
2024-11-17 16:44:01 -0800

    Header made responsive

--- src/components/SiteHeader/centerHeader.tsx
+++ src/components/SiteHeader/centerHeader.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React from 'react'
-import { Smartphone } from "lucide-react"
+import { Bell } from "lucide-react"
 import Link from "next/link"
 
 export default function CenterHeader() {
+    const screenWidth = window.screen.width
   return (
     <div className="flex justify-center items-center gap-2 text-[#FFFFFF]">
-        <Link href="/"> <Smartphone /> </Link>
-        <Link href="/"> <span className='underline'>Click here to subscribe to the UBC Events Newsletter!</span></Link>
+        <Link href="/" className='hidden md:flex'> <Bell size={(screenWidth >= 1024) ? 16: 12}/> </Link>
+        <Link href="/"> <span className='underline hidden md:flex sm:text-xs lg:text-base text-center'>Subscribe to the UBC Events Newsletter!</span></Link>
     </div>
   )
 }
