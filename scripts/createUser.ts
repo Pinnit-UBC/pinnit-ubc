4775020eb085f67496634116165a4117944bef59
YOUR NAME HERE <richardluo73@gmail.com>
2024-11-19 00:38:47 -0800

    update to adding user

--- scripts/createUser.ts
+++ scripts/createUser.ts
@@ -1,12 +1,16 @@
+import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 import connectMongo from '../src/lib/db'; // Correct path to db.ts
 import User from '../src/models/User'; // Correct path to User model
+
+// Load environment variables
+dotenv.config({ path: '.env.local' });
 
 async function createUser() {
   try {
     await connectMongo();
 
-    const email = 'test@example.com'; // Replace with your desired email
+    const email = 'testing2@example.com'; // Replace with your desired email
     const plainPassword = 'password123'; // Replace with your desired password
 
     const hashedPassword = await bcrypt.hash(plainPassword, 10);
