a387e52e738cd8d50311319fa52b9d8680b360fa
YOUR NAME HERE <richardluo73@gmail.com>
2024-11-29 16:09:26 -0800

    added type to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../app/globals.css'; // Adjust the path if necessary
+import type { AppProps } from 'next/app'; // Import the type for AppProps
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
