5291a948690f8565e630ed5e5acb99d4b524d520
YOUR NAME HERE <richardluo73@gmail.com>
2024-12-05 01:22:28 -0800

    Commit

--- src/app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import AuthPage from "@/components/auth-page";
-import React from "react";
-
-const page = () => {
-  return <AuthPage />;
-};
-
-export default page;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
 
     const response = await fetch('/api/auth/login', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' }, 
       body: JSON.stringify({ email, password }),
     });
 
